1c4079d9b08619f3a78d1467468c5e55e1b8dc10
Ola Ghoneim <olaghoneim38@gmail.com>
2026-02-01 04:55:38 +0200

    [feat] Add Spring bean scopes (singleton/prototype) and lifecycle (init/destroy) configuration

---
+++ Task3/task2/src/com/DatabaseOperations.java
@@ -0,0 +1,10 @@
+package com;
+
+public class DatabaseOperations {
+    public void connectToDatabase() {
+      System.out.println("Connecting to database...");
+    }
+    public void disconnectFromDatabase() {
+        System.out.println("Disconnecting from database...");
+    }
+}
